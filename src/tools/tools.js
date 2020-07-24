68d391bfc01bbb7432588b6c803925c4fb33fb14
dnPC <denisagafonovwork.gmail.com>
2020-07-24 18:52:54 +0300

    fix: BL fake request error

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,2 +1,2 @@
 export const normalizePath = path => path.split(/\/+$/)[0];
-export const sleep = (data, ms) => new Promise(() => setTimeout(() => data, ms))
+export const sleep = (data, ms) => new Promise(r => setTimeout(() => r(data), ms))
