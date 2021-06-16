9a37a71845d5ec38bf54b8d86df2a06ac57effa6
Denis Agafonov <denisagafonovwork@gmail.com>
2021-06-17 02:11:13 +0300

    diplom

--- src/components/header-logo/header-logo.jsx
+++ src/components/header-logo/header-logo.jsx
@@ -9,26 +9,29 @@ import HeaderLogoTitle from "./header-logo__title/header-logo__title";
 
 import "./header-logo.scss";
 import HeaderNav from "../header/-nav/header-nav";
+import Wrapper from "../common/wrapper/wrapper";
 
 const HeaderLogo = ({ openDrawer, visible }) => (
-  <div className="header-logo media1220">
-    <Link to="/" className="link">
-      <Image src={logo} alt="logo" width="130px" />
-    </Link>
-    <Button onClick={openDrawer}>
-      <UnorderedListOutlined />
-    </Button>
-    <Drawer
-      className="drawer"
-      title={<HeaderLogoTitle openDrawer={openDrawer} />}
-      placement="right"
-      closable={false}
-      onClose={openDrawer}
-      visible={visible}
-    >
-      <HeaderNav />
-    </Drawer>
-  </div>
+  <Wrapper>
+    <div className="header-logo media1220">
+      <Link to="/" className="link">
+        <Image src={logo} alt="logo" width="130px" />
+      </Link>
+      <Button onClick={openDrawer}>
+        <UnorderedListOutlined />
+      </Button>
+      <Drawer
+        className="drawer"
+        title={<HeaderLogoTitle openDrawer={openDrawer} />}
+        placement="right"
+        closable={false}
+        onClose={openDrawer}
+        visible={visible}
+      >
+        <HeaderNav />
+      </Drawer>
+    </div>
+  </Wrapper>
 );
 
 export default HeaderLogo;
